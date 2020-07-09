78c73bea01295b88680d005c061cd8ddccef707b
Grigory <grigrol48@mail.ru>
2020-07-09 16:20:40 +0300

    изменил мейн, чтобы убрать создание строки на каждой итерации цикла

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "FileMover.h"
 #include "ConfigReader.h"
 
-
 int main() {
     FileWorker fw;
     Parser p;
@@ -15,16 +14,19 @@ int main() {
 
     std::filesystem::path path = std::filesystem::current_path();
     std::string parent_path = path.parent_path().string();
+
     Paths paths = cr.ReadPaths(parent_path + "/config.txt");
+    std::string input_path = parent_path + paths.input_path_;
+    std::string delete_path = parent_path + paths.delete_path_;
 
     while (true) {
-        if (!fw.ReadFileToBuf(parent_path + paths.input_path_)) {
+        if (!fw.ReadFileToBuf(input_path)) {
             std::this_thread::sleep_for(std::chrono::seconds(3));
             continue;
         }
 
         fw.SetOutputBuf(p.Parse(fw.GetInputBuf()));
-        fm.ToDelete(parent_path + paths.input_path_, parent_path + paths.delete_path_);
+        fm.ToDelete(input_path, delete_path);
     }
     return 0;
 }
